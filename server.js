b4951424d49d84dce824b041395d295eaae1084c
abby <abby@vradars.com>
2019-12-18 17:54:19 +0530

    Added Master-Worker Node Processes

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+process.title = "NSFCAREER";
+
+// Include the cluster module
+const cluster = require('cluster');
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+    // Listen for terminating workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the terminated workers
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
+    // Code to run if we're in a worker process
+} else {
+
+
+
 // ======================================
 //         INITIALIZING DEPENDENCIES
 // ======================================
@@ -2666,3 +2696,4 @@ app.post(`${apiPrefix}IRBFormGenerate`, function(req, res){
         const server = app.listen(port, function () {
             console.log('Magic happens on ' + port);
         });
+}
